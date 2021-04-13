b14fb47d14a4e51e5a906d058d395fafbbdc8621
Kunal <kunaldas1244@gmail.com>
2021-04-14 00:58:27 +0530

    json repped

--- src/app.js
+++ src/app.js
@@ -10,7 +10,21 @@ document.getElementById("lastfm").onsubmit = async (e) => {
         },
         body : JSON.stringify(data)
     }).then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        for(let i in data["albums"]){
+            var node = document.createElement("li");
+            var textnode = document.createTextNode(data["albums"][i]);
+            node.appendChild(textnode);
+            document.getElementById("albums").appendChild(node);
+        }
+        for(let i in data["artists"]){
+            var node = document.createElement("li");
+            var textnode = document.createTextNode(data["artists"][i]);
+            node.appendChild(textnode);
+            document.getElementById("artists").appendChild(node);
+        }
+    })
 }
 
 
@@ -27,7 +41,6 @@ document.getElementById("get-recs").onclick = async (e) => {
 document.getElementById("create-playlist").onclick = async (e) => {
     e.preventDefault();
     fetch('http://localhost:3030/login', {
-        mode : 'no-cors', 
         method : 'GET',
         headers : {
             'Content-Type' : 'application/json'
